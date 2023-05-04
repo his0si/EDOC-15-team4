cacf92ecff52ca481d872d8c7967796eaedda494
Hee Seo Kim <hisounni@ewhain.net>
2023-05-04 09:27:51 +0900

    5/4

--- 1260.py
+++ 1260.py
@@ -1,6 +1,37 @@
-N,M,V = map(int, input().split())
+from collections import deque
+import sys
 
-adj=[0 for _ in range(N) for _ in range(N)]
-#간선으로 연결된 두 정점은 관계가 있다고 말할 수 있으며, 이를 인접(Adjacent)하다고 한다.
+input=sys.stdin.readline
+
+n,m,v=map(int,input().split())
+link=[[0]*(n+1) for _ in range(n+1)] #간선 행렬로 표현
+visit=[0]*(n+1)
+#default값은 0; 연결되거나 지나갔을경우 1
 
 
+for i in range(m):
+    x,y=map(int,input().split())
+    link[x][y]=link[y][x]=1
+
+def dfs(v):
+    visit[v]=1
+    print(v,end=' ')
+    for i in range(1,n+1):
+        if(visit[i]==0 and link[v][i]==1): #방문안했고, 연결되있을 시
+            dfs(i)
+
+def bfs(v):
+    visit[v]=0 #dfs실행 이후 default가 1이 됨->방문하면 0으로
+    q=[v]
+
+    while q:
+        v=q.pop(0)
+        print(v,end=' ')
+        for i in range(1,n+1):
+            if(visit[i]==1 and link[v][i]==1):
+                q.append(i)
+                visit[i]=0
+
+dfs(v)
+print()
+bfs(v)
