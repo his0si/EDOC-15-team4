e6f7e1cf90c5104b058382dcaa3c4f9ca8a33abe
Hee Seo Kim <hisounni@ewhain.net>
2023-05-25 10:14:46 +0900

    5/25

--- 10816.py
+++ 10816.py
@@ -1,25 +1,27 @@
-N = int(input())
-card = list(map(int, input().split()))
-M = int(input())
-Mcard = list(map(int, input().split()))
-ans = []
+import sys
+input = sys.stdin.readline
 
-for i in range (M):
-    ans[i]=0  # 리스트 초기화
+N = int(input())
+card = list(map(int, sys.stdin.readline().split()))
+M = int(input())
+Mcard = list(map(int, sys.stdin.readline().split()))
+ans = [0] * M  # ans 리스트 초기화
 
 card.sort()  # card 리스트 정렬(리스트 내장 함수)
 
-for i in range(M):
-    left = 0
-    right = N - 1
+def binary_search(target, left, right):
     while left <= right:
-        mid = (left + right) // 2
-        if card[mid] == Mcard[i]:
-            ans[i] += 1
-            break
-        elif card[mid] < Mcard[i]:
-            left = mid + 1
+        mid = (left + right) // 2  # 중간 인덱스 계산
+        if card[mid] == target:  # 타깃 카드 발견
+            return True
+        elif card[mid] < target:  # 중간 값이 타겟 카드보다 작을 경우
+            left = mid + 1  # 오른쪽 부분 탐색
         else:
-            right = mid - 1
+            right = mid - 1  # 왼쪽 부분 탐색
+    return False
 
-print(*ans)  #print(*ans)는 ans 리스트의 요소들을 공백으로 구분하여 출력
+for i in range(M):
+    if binary_search(Mcard[i], 0, N - 1):
+        ans[i] = card.count(Mcard[i])  # 해당 숫자의 개수를 세서 저장
+
+print(*ans)  # ans 리스트의 요소들을 공백으로 구분하여 출력
---
+++ 10816_2.py
@@ -0,0 +1,17 @@
+from collections import Counter
+# Counter 클래스는 collections 모듈에서 제공
+# 입력된 순서대로 요소를 카운트하고 딕셔너리 형태로 반환한다
+# 이를 활용하여 각 숫자 카드의 개수를 세는데 활용 가능
+
+N = int(input())
+card = list(map(int, input().split()))
+M = int(input())
+Mcard = list(map(int, input().split()))
+
+card_counter = Counter(card)  # 카드의 숫자를 세는 Counter 객체 생성
+
+ans = []
+for num in Mcard:
+    ans.append(card_counter[num])
+
+print(*ans)
