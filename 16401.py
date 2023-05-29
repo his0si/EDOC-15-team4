bfa92284146fb077ff5344d5d109f1e5c4f1cfeb
Hee Seo Kim <hisounni@ewhain.net>
2023-05-29 17:10:26 +0900

    5/29

--- 16401.py
+++ 16401.py
@@ -1,7 +1,28 @@
-import sys
+M, N = map(int, input().split())
+cookies = list(map(int, input().split()))
 
-M, N = map(int, sys.stdin.readline().split())
-snack = list(map(int, sys.stdin.readline().split()))
 
-start = 0
-end = snack[N-1]
+def binary_search(cookies, length):
+    count = 0
+    for cookie in cookies:
+        count = count + cookie // length
+    return count
+
+cookies.sort()
+
+start = 1
+end = cookies[-1]
+
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = binary_search(cookies, mid)
+
+    if count >= M:
+        result = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(result)
